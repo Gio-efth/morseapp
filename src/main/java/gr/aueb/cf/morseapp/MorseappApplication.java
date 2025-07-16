d0899ac6d4876f59329f69c175a32e63458c32ea
Efth. Giorgos <geoefthimiou26@gmail.com>
2025-07-16 19:55:32 +0300

    init commit

---
+++ src/main/java/gr/aueb/cf/morseapp/MorseappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.morseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MorseappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MorseappApplication.class, args);
+	}
+
+}
