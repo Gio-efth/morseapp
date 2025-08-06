a47d5f8b50f4374664061af570a88025b19be254
Efth. Giorgos <geoefthimiou26@gmail.com>
2025-08-06 12:43:04 +0300

    application.properties: Logging & Debug added build.gradle: Fix:changed plugin version OpenApiConfig: Updated Swagger config

--- src/main/java/gr/aueb/cf/morseapp/config/OpenApiConfig.java
+++ src/main/java/gr/aueb/cf/morseapp/config/OpenApiConfig.java
@@ -2,10 +2,10 @@ package gr.aueb.cf.morseapp.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.*;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,36 +16,20 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 @OpenAPIDefinition(
-        info = @io.swagger.v3.oas.annotations.info.Info(
+        info = @Info(
                 title = "MorseApp API",
                 version = "1.0.0",
-                description = "API τεκμηρίωση για μετατροπή κειμένου - κώδικα Morse",
-                contact = @io.swagger.v3.oas.annotations.info.Contact(
+                description = "REST API για μετατροπή κειμένου - κώδικα Morse με Authentication",
+                contact = @Contact(
                         name = "MorseApp Dev Team",
                         email = "developer@morseapp.gr",
                         url = "https://cf.gr"
                 ),
-                license = @io.swagger.v3.oas.annotations.info.License(
+                license = @License(
                         name = "MIT License",
                         url = "https://opensource.org/licenses/MIT"
                 )
         )
 )
 public class OpenApiConfig {
-
-    @Bean
-    public OpenAPI customOpenAPI() {
-        return new OpenAPI()
-                .info(new Info()
-                        .title("MorseApp API")
-                        .version("1.0.0")
-                        .description("REST API για μετατροπή κώδικα Morse - Κείμενο με Authentication")
-                        .contact(new Contact()
-                                .name("Coding Factory")
-                                .email("support@cf.gr")
-                                .url("https://cf.gr"))
-                        .license(new License()
-                                .name("MIT License")
-                                .url("https://opensource.org/licenses/MIT")));
-    }
 }
