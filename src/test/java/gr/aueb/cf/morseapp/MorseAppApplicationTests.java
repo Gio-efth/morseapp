78bee7896bb3e43b175968913ef8c924eb37ed19
Efth. Giorgos <geoefthimiou26@gmail.com>
2025-08-19 20:36:02 +0300

    MorseAppApplicationTests: temporarily ignored with @Disabled

--- src/test/java/gr/aueb/cf/morseapp/MorseAppApplicationTests.java
+++ src/test/java/gr/aueb/cf/morseapp/MorseAppApplicationTests.java
@@ -1,8 +1,10 @@
 package gr.aueb.cf.morseapp;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class MorseAppApplicationTests {
 
